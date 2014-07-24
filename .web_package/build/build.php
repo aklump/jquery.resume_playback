ed20c3297d88c0dd6caddfb00ad4e0544c552f20
aklump <sourcecode@intheloftstudios.com>
2014-07-23 18:20:48 -0700

    Initial import.

---
+++ .web_package/build/build.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @file
+ * Embeds .info file information into the jquery plugin file itself.
+ *
+ * The header of the jquery file must look like the following example, line for line exact.
+ *
+ * Be sure to replace the $source_file with the correct filename
+ */
+
+//Here is the header required to use this script...
+/*
+ * {{ name }} jQuery JavaScript Plugin v{{ version }}
+ * {{ homepage }}
+ *
+ * {{ description }}.
+ *
+ * Copyright 2013, {{ name }}
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Date: {{ date }}
+ */
+
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+
+// Translate arguments to vars.
+list(
+  $this_file,
+  $prev_version,
+  $new_version,
+  $package_name,
+  $description,
+  $homepage,
+  $author,
+  $path_to_root,
+  $date
+) = $argv;
+
+// Get the source file of the jquery plugin.
+$source_file = $path_to_root . '/jquery.resume_playback.js';
+$source = file_get_contents($source_file);
+
+// Pull out only comment lines for manipulation to protect code.
+preg_match_all("/(\/| )?\*.*$/m", $source, $matches);
+$comment_lines = $comment_lines_replace = $matches[0];
+
+// Target each comment line based on convention
+js_replace_name_version($comment_lines_replace[1], $package_name, $new_version);
+js_replace_homepage($comment_lines_replace[2], $homepage);
+js_replace_description($comment_lines_replace[4], $description);
+js_replace_date($comment_lines_replace[9], $date);
+
+// Replace the old comment block with new one.
+$find     = implode(PHP_EOL, $comment_lines);
+$replace  = implode(PHP_EOL, $comment_lines_replace);
+$source   = str_replace($find, $replace, $source);
+
+// Save the new version of the file
+if (file_put_contents($source_file, $source)) {
+  echo "$source_file has been updated.";
+}
+else {
+  echo "Error updating $source_file";
+}
---
+++ jquery.resume_playback.js
@@ -0,0 +1,181 @@
+/**
+ * {{ name }} jQuery JavaScript Plugin v{{ version }}
+ * {{ homepage }}
+ *
+ * {{ description }}.
+ *
+ * Copyright 2013, Aaron Klump
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Date: {{ date }}
+ */
+;(function($, window, document, undefined) {
+"use strict";
+
+// The actual plugin constructor
+function ResumePlayback(element, id, options) {
+  this.element = element;
+
+  // jQuery has an extend method that merges the 
+  // contents of two or more objects, storing the 
+  // result in the first object. The first object 
+  // is generally empty because we don't want to alter 
+  // the default options for future instances of the plugin
+  this.options = $.extend( {}, defaults, options) ;
+  
+  this._defaults = $.fn.resumePlayback.defaults;
+  // this._name = 'resumePlayback';
+  
+  this.init();
+
+  var nonceStorage = null;
+
+  // // Create some defaults, extending them with any options that were provided
+  // var settings = $.extend( {
+  //   'refresh'         : 1000,
+  //   'resuming'        : null,
+  //   'resumed'         : null,
+  //   'dataStore'       : dataStoreCookies,
+  //   'dataStoreKey'    : 'resumePlayback',
+  //   'dataLifetime'    : 365
+  // }, options);
+
+  /**
+   * Event handler for various video events: captures playhead position.
+   *
+   * @param  {object} e The event object.
+   */
+  this.element.bind('play seeked pause abort timeupdate', function (e) {
+    var nonce = e.target.currentTime;
+    if (nonce !== nonceStorage && nonce > 0) {
+      settings.dataStore.set(id, nonce);
+      nonceStorage = nonce;
+    };
+  });    
+}
+
+ResumePlayback.prototype.init = function () {
+  if ((var resumeAt = this.options.dataStore.get(id))) {
+
+    // Pre-resume callback.
+    if (this.options.resuming) {
+      this.options.resuming(this.element, resumeAt);
+    }
+
+    var bindEvents = 'canplaythrough play';
+
+    // When the page loads, depending on the device the canplay event may
+    // fire.  If it does then we set the playhead position.  If it doesn't
+    // we set the playhead position the first time it plays.  In either
+    // case we immediately unbind this event, as this is a one time thing.
+    var initialResumePlayhead = function (e) {
+      var time = e.target.currentTime;
+      e.target.currentTime = resumeAt;
+      this.element.unbind(bindEvents, initialResumePlayhead);
+      
+      // Post resume callback.
+      if (this.options.resumed) {
+        this.options.resumed(this.element, resumeAt);
+      }
+    };
+    
+    this.element.bind(bindEvents, initialResumePlayhead);
+  }
+};
+
+$.fn.resumePlayback = function(id, options) {
+  return this.each(function () {
+    if (!$.data(this, 'plugin_resumePlayback')) {
+      $.data(this, 'plugin_resumePlayback', 
+      new ResumePlayback(this, id, options));
+    }
+  });
+};
+
+$.fn.resumePlayback.defaults = {
+  'refresh'           : 1000,
+  'resuming'          : null,
+  'resumed'           : null,
+  'dataStore'         : dataStoreCookies,
+  'dataStoreKey'      : 'resumePlayback',
+  'dataLifetime'      : 365
+  
+  // A prefix for all css classes
+  "cssPrefix"         : 'resume-playback'  
+};
+
+$.fn.resumePlayback.global = {};
+
+$.fn.resumePlayback.somePublicMethod  = function () {
+
+};
+
+/**
+ * Default data storage uses native browser cookies.
+ *
+ * We use a single cooked with the name settings.dataStoreKey to store
+ * a json object.
+ *
+ * @type {Object}
+ */
+var dataStoreCookies = {
+
+  /**
+   * Sets the value of name in persistent storage
+   *
+   * @param {string} name
+   * @param {mixed} value
+   */
+  set: function(name, value) {
+
+    // Merge this time value into our cookies array of all storage keys.
+    var stored = this.get();
+    stored[name] = value;
+    stored = JSON.stringify(stored);
+
+    // No set just one cookies.
+    var name = settings.dataStoreKey;
+    var days = settings.dataLifetime;
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+stored+expires+"; path=/";    
+  },
+
+  /**
+   * Gets the value by key
+   *
+   * @param  {string} name The unique key for this storage data.  Omit this
+   * value and the entire data store will be returned.
+   *
+   * @return {mixed}
+   */
+  get: function(name) {
+    var value = 0;
+    var nameEQ = settings.dataStoreKey + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) {
+        value = c.substring(nameEQ.length,c.length);
+      } 
+    }
+
+    var raw = JSON.parse(value) || {};
+    if (!name) {
+      return raw;
+    };
+
+    var value = typeof raw[name] === 'undefined' ? 0 : raw[name];
+
+    return value;
+  },
+}   
+
+$.fn.resumePlayback.version = function() { return '0.0.1'; };
+
+})(jQuery, window, document);
